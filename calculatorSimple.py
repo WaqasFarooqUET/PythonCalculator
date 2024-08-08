d1f85285da6862d6f7e3e0e72248baf359ece7e3
WaqasFarooqUET <waqas.farooq.uet@gmail.com>
2024-08-08 09:37:56 +0500

    text change
    text change

--- calculatorSimple.py
+++ calculatorSimple.py
@@ -1,24 +1,24 @@
 import streamlit as st
 
 # Title of the app
-st.title('Simple Calculator')
+st.title('Simple Calculator using Python')
 
 # Input fields for numbers
-num1 = st.number_input('Enter the first number', value=0.0)
-num2 = st.number_input('Enter the second number', value=0.0)
+num1 = st.number_input('Enter the first number value here', value=0.0)
+num2 = st.number_input('Enter the second number value here', value=0.0)
 
 # Drop-down menu for operation selection
-operation = st.selectbox('Choose an operation', ['Add', 'Subtract', 'Multiply', 'Divide'])
+operation = st.selectbox('Choose an operation to calculate', ['Add', 'Subtract', 'Multiply', 'Divide'])
 
 # Calculate result based on selected operation
 result = None
-if operation == 'Add':
+if operation == 'Addition':
     result = num1 + num2
-elif operation == 'Subtract':
+elif operation == 'Subtraction':
     result = num1 - num2
-elif operation == 'Multiply':
+elif operation == 'Multiplication':
     result = num1 * num2
-elif operation == 'Divide':
+elif operation == 'Division':
     if num2 != 0:
         result = num1 / num2
     else:
