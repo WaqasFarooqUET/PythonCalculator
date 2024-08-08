c5fc599fd925d23a0c1299061955cd3715df7e78
WaqasFarooqUET <waqas.farooq.uet@gmail.com>
2024-08-08 09:24:01 +0500

    hh
    hh

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mac/Documents/GitHub/PythonCalculator/PythonCalculator/myenv"
+set -gx VIRTUAL_ENV "/Users/mac/Documents/GitHub/PythonCalculator/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
